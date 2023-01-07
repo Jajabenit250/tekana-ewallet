a18054582bd0a15d2b49ceb0997c2ac6685b3403
Jajabenit250 <jajabenit@gmail.com>
2023-01-08 00:12:22 +0200

    fixes docker issues

--- microservices/tekana-customer-svc/src/customer/customer.entity.ts
+++ microservices/tekana-customer-svc/src/customer/customer.entity.ts
@@ -1,49 +1,48 @@
 import { Exclude } from 'class-transformer';
 import {
-    BaseEntity,
-    Column,
-    Entity,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 // redo for better
 @Entity()
 export class Customer extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    public id!: number;
+  @PrimaryGeneratedColumn()
+  public id!: number;
 
-    @Column({ type: 'varchar', unique: true, length: 50 })
-    public email!: string;
+  @Column({ type: 'varchar', unique: true, length: 50 })
+  public email!: string;
 
-    @Column({ type: 'varchar', length: 50 })
-    public fullName!: string;
+  @Column({ type: 'varchar', length: 50 })
+  public fullName!: string;
 
-    @Column({ type: 'varchar', length: 50 })
-    public nationalId!: string;
+  @Column({ type: 'varchar', length: 50 })
+  public nationalId!: string;
 
-    @Column('enum', {
-        name: 'gender',
-        nullable: true,
-        enum: ['M', 'F'],
-        default: 'M',
-    })
-    public gender!: 'M' | 'F' | null;
+  @Column('enum', {
+    name: 'gender',
+    nullable: true,
+    enum: ['M', 'F'],
+    default: 'M',
+  })
+  public gender!: 'M' | 'F' | null;
 
+  @Exclude()
+  @Column({ type: 'varchar', length: 100 })
+  public password!: string;
 
-    @Exclude()
-    @Column({ type: 'varchar', length: 100 })
-    public password!: string;
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
 
-    @Column()
-    @CreateDateColumn()
-    created_at: Date;
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
 
-    @Column()
-    @UpdateDateColumn()
-    updated_at: Date;
-
-    @Column('timestamp with time zone', { name: 'deleted_at', nullable: true })
-    deletedAt: Date | null;
+  @Column('timestamp with time zone', { name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
 }
--- microservices/tekana-transaction-svc/src/app.module.ts
+++ microservices/tekana-transaction-svc/src/app.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
+      port: parseInt(process.env.DB_PORT, 10),
       database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
